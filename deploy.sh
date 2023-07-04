e7beddb7976d6807476cb6a6294b85f44bd1d2ab
xiaodongxu <lingyidx@gmail.com>
2023-07-04 10:57:50 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+echo 'git add -A;'
 git add -A;
+echo "git commit -m 'update';"
 git commit -m 'update';
+echo 'git push origin main;'
 git push origin main;
+echo 'hexo clean;'
 hexo clean;
+echo 'hexo generate;'
 hexo generate;
+echo 'hexo deploy;'
 hexo deploy;
